80249505bd18ec0f229b3ef2f262fa3ef9e12a1d
Cornelius Wiehl <cw@ai.associates>
2023-01-07 15:08:54 +0100

    included mroe leagues

--- scraping/climbing_scraper.py
+++ scraping/climbing_scraper.py
@@ -9,78 +9,95 @@ import pandas as pd
 import tqdm
 import time
 
-EVENT_URL = 'https://components.ifsc-climbing.org/results-api.php?api=event_results&event_id={}'
-RESULT_URL = 'https://components.ifsc-climbing.org/results-api.php?api=event_full_results&result_url={}'
+EVENT_URL = (
+    "https://components.ifsc-climbing.org/results-api.php?api=event_results&event_id={}"
+)
+RESULT_URL = "https://components.ifsc-climbing.org/results-api.php?api=event_full_results&result_url={}"
 
-data = {'id': [], 'name': [], 'league_id': [], 
-                'league_season_id': [], 'season_id': [], 
-                'starts_at': [], 'ends_at': [], 'location': [], 
-                'dcat_name': [], 'discipline_kind': [], 
-                'category_name': [], 'rank': [], 'athlete_id': [], 
-                'firstname': [], 'lastname': [], 
-                'paraclimbing_sport_class': [], 
-                'country': [], 'category_round_id': [], 
-                'round_name': [], 'score': [], }
+data = {
+    "id": [],
+    "name": [],
+    "league_id": [],
+    "league_season_id": [],
+    "season_id": [],
+    "starts_at": [],
+    "ends_at": [],
+    "location": [],
+    "dcat_name": [],
+    "discipline_kind": [],
+    "category_name": [],
+    "rank": [],
+    "athlete_id": [],
+    "firstname": [],
+    "lastname": [],
+    "paraclimbing_sport_class": [],
+    "country": [],
+    "category_round_id": [],
+    "round_name": [],
+    "score": [],
+}
 
 steps = 4000
-for i in tqdm.tqdm(range(50,steps), total=steps):
+for i in tqdm.tqdm(range(50, steps), total=steps):
     try:
         json_event = requests.get(EVENT_URL.format(i)).json()
-        id = json_event['id']
-        name = json_event['name']
+        id = json_event["id"]
+        name = json_event["name"]
         # filter out non-IFSC events
-        if 'IFSC' not in name:
+        if "IFSC" not in name:
             continue
-        league_id = json_event['league_id']
-        league_season_id = json_event['league_season_id']
-        season_id = json_event['season_id']
-        starts_at = json_event['starts_at']
-        ends_at = json_event['ends_at']
-        location = json_event['location']
-        for event in json_event['d_cats']:
-            dcat_name = event['dcat_name']
-            discipline_kind = event['discipline_kind']
-            category_name = event['category_name']
-            event_results = requests.get(RESULT_URL.format(event['full_results_url'])).json()
-            for athlete in event_results['ranking']:
-                rank = athlete['rank']
-                athlete_id = athlete['athlete_id']
-                firstname = athlete['firstname']
-                lastname = athlete['lastname']
-                paraclimbing_sport_class = athlete['paraclimbing_sport_class']
-                country = athlete['country']
-                for round in athlete['rounds']:
-                    category_round_id = round['category_round_id']
-                    round_name = round['round_name']
-                    score = round['score']
+        league_id = json_event["league_id"]
+        league_season_id = json_event["league_season_id"]
+        season_id = json_event["season_id"]
+        starts_at = json_event["starts_at"]
+        ends_at = json_event["ends_at"]
+        location = json_event["location"]
+        for event in json_event["d_cats"]:
+            dcat_name = event["dcat_name"]
+            discipline_kind = event["discipline_kind"]
+            category_name = event["category_name"]
+            event_results = requests.get(
+                RESULT_URL.format(event["full_results_url"])
+            ).json()
+            for athlete in event_results["ranking"]:
+                rank = athlete["rank"]
+                athlete_id = athlete["athlete_id"]
+                firstname = athlete["firstname"]
+                lastname = athlete["lastname"]
+                paraclimbing_sport_class = athlete["paraclimbing_sport_class"]
+                country = athlete["country"]
+                for round in athlete["rounds"]:
+                    category_round_id = round["category_round_id"]
+                    round_name = round["round_name"]
+                    score = round["score"]
                     # append all variables to data
-                    data['id'].append(id)
-                    data['name'].append(name)
-                    data['league_id'].append(league_id)
-                    data['league_season_id'].append(league_season_id)
-                    data['season_id'].append(season_id)
-                    data['starts_at'].append(starts_at)
-                    data['ends_at'].append(ends_at)
-                    data['location'].append(location)
-                    data['dcat_name'].append(dcat_name)
-                    data['discipline_kind'].append(discipline_kind)
-                    data['category_name'].append(category_name)
-                    data['rank'].append(rank)
-                    data['athlete_id'].append(athlete_id)
-                    data['firstname'].append(firstname)
-                    data['lastname'].append(lastname)
-                    data['paraclimbing_sport_class'].append(paraclimbing_sport_class)
-                    data['country'].append(country)
-                    data['category_round_id'].append(category_round_id)
-                    data['round_name'].append(round_name)
-                    data['score'].append(score)
+                    data["id"].append(id)
+                    data["name"].append(name)
+                    data["league_id"].append(league_id)
+                    data["league_season_id"].append(league_season_id)
+                    data["season_id"].append(season_id)
+                    data["starts_at"].append(starts_at)
+                    data["ends_at"].append(ends_at)
+                    data["location"].append(location)
+                    data["dcat_name"].append(dcat_name)
+                    data["discipline_kind"].append(discipline_kind)
+                    data["category_name"].append(category_name)
+                    data["rank"].append(rank)
+                    data["athlete_id"].append(athlete_id)
+                    data["firstname"].append(firstname)
+                    data["lastname"].append(lastname)
+                    data["paraclimbing_sport_class"].append(paraclimbing_sport_class)
+                    data["country"].append(country)
+                    data["category_round_id"].append(category_round_id)
+                    data["round_name"].append(round_name)
+                    data["score"].append(score)
         time.sleep(0.05)
     except requests.exceptions.JSONDecodeError as e:
         continue
     except Exception as e:
         print(e)
         continue
-    
-    
+
+
 df = pd.DataFrame(data)
-df.to_csv('../data/raw_data/climbing_data.csv', index=False)
+df.to_csv("../data/raw_data/climbing_data.csv", index=False)
--- scraping/esports_scraper.py
+++ scraping/esports_scraper.py
@@ -7,53 +7,63 @@ import tqdm
 import time
 from bs4 import BeautifulSoup
 
-CSGO_URL = 'https://www.gosugamers.net/counterstrike/matches/list/results?maxResults=100&page={}'
-LOL_URL = 'https://www.gosugamers.net/lol/matches/list/results?maxResults=100&page={}'
-DOTA_URL = 'https://www.gosugamers.net/dota2/matches/list/results?maxResults=100&page={}'
+CSGO_URL = "https://www.gosugamers.net/counterstrike/matches/list/results?maxResults=100&page={}"
+LOL_URL = "https://www.gosugamers.net/lol/matches/list/results?maxResults=100&page={}"
+DOTA_URL = (
+    "https://www.gosugamers.net/dota2/matches/list/results?maxResults=100&page={}"
+)
 steps = 4500
 
-def scrape(URL, filename):    
+
+def scrape(URL, filename):
     # iterate through pages
-    for i in tqdm.tqdm(range(0,steps), total=steps):
-        data = {'match_date': [], 'team': [], 'score': [], 'tournament': [], 'win': []}
+    for i in tqdm.tqdm(range(0, steps), total=steps):
+        data = {"match_date": [], "team": [], "score": [], "tournament": [], "win": []}
         response = requests.get(URL.format(i))
         # parse html
-        bs = BeautifulSoup(response.content, 'html.parser')
-        match_list = bs.find('div', {'class':'match-list'})
+        bs = BeautifulSoup(response.content, "html.parser")
+        match_list = bs.find("div", {"class": "match-list"})
         # iterate through matches
         for match in match_list.findChildren(recursive=False)[:-1]:
             # get match date
-            if match.has_attr('class') and 'match-date' in match['class']:
+            if match.has_attr("class") and "match-date" in match["class"]:
                 match_date = match.text.strip()
                 continue
             # get match info
-            elif match.has_attr('class') and 'cell' in match['class'] and len(match['class']) == 1:
-                info = match.find('div',{'class':'match-info'})
-                team1 = info.find('span',{'class':'team-1'}).text.strip()
-                team2 = info.find('span',{'class':'team-2'}).text.strip()
-                tournament = info.find('div',{'class':'match-tournament'}).text.strip()
-                score = match.find('div',{'class':'match-score'})
-                spans = score.find_all('span')
+            elif (
+                match.has_attr("class")
+                and "cell" in match["class"]
+                and len(match["class"]) == 1
+            ):
+                info = match.find("div", {"class": "match-info"})
+                team1 = info.find("span", {"class": "team-1"}).text.strip()
+                team2 = info.find("span", {"class": "team-2"}).text.strip()
+                tournament = info.find(
+                    "div", {"class": "match-tournament"}
+                ).text.strip()
+                score = match.find("div", {"class": "match-score"})
+                spans = score.find_all("span")
                 scores = [span.text.strip() for span in spans]
-                classes = [span['class'][0] for span in spans]
+                classes = [span["class"][0] for span in spans]
                 team1_score = scores[0]
                 team2_score = scores[2]
                 team1_win = classes[0]
                 team2_win = classes[2]
-                data['match_date'].append(match_date)
-                data['team'].append(team1)
-                data['score'].append(team1_score)
-                data['tournament'].append(tournament)
-                data['win'].append(team1_win)
-                data['match_date'].append(match_date)
-                data['team'].append(team2)
-                data['score'].append(team2_score)
-                data['tournament'].append(tournament)
-                data['win'].append(team2_win)
-                
+                data["match_date"].append(match_date)
+                data["team"].append(team1)
+                data["score"].append(team1_score)
+                data["tournament"].append(tournament)
+                data["win"].append(team1_win)
+                data["match_date"].append(match_date)
+                data["team"].append(team2)
+                data["score"].append(team2_score)
+                data["tournament"].append(tournament)
+                data["win"].append(team2_win)
+
         df = pd.DataFrame(data)
-        df.to_csv(filename, mode='a', header=False, index=False)
-        
-scrape(CSGO_URL, '../data/raw_data/csgo_matches.csv')
-scrape(LOL_URL, '../data/raw_data/lol_matches.csv')
-scrape(DOTA_URL, '../data/raw_data/dota_matches.csv')
+        df.to_csv(filename, mode="a", header=False, index=False)
+
+
+scrape(CSGO_URL, "../data/raw_data/csgo_matches.csv")
+scrape(LOL_URL, "../data/raw_data/lol_matches.csv")
+scrape(DOTA_URL, "../data/raw_data/dota_matches.csv")
